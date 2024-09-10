338c91479ee480bd6fe55ff153f3a91a831ced2a
johan66789 <kimouappiah2000@gmail.com>
2024-09-10 09:14:43 +0000

    update successfuly

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for,session,flash
 from flask_wtf import FlaskForm
 from flask_wtf.csrf import CSRFProtect
 from wtforms import StringField, PasswordField, SubmitField
@@ -29,6 +29,13 @@ class RegisterForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Register")
 
+
+# LoginForm
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("password",validators=[DataRequired()])
+    submit = SubmitField("Register")
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -52,9 +59,26 @@ def register():
 
     return render_template('register.html', form_register=form_register)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    form_login = LoginForm()
+    if form_login.validate_on_submit():
+        email = form_login.email.data
+        password = form_login.password.data
+
+        req_login = mysql.connection.cursor()
+        req_login.execute("SELECT * FROM utilisateur WHERE email=%s",(email,))
+        user = req_login.fetchone()
+        req_login.close()
+
+        if user and bcrypt.checkpw(password.encode('utf-8'), user[3].encode('utf-8')):
+            session['user_id'] = user[0]
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Connexion échouée")
+            return redirect(url_for('login'))
+
+    return render_template('login.html',form_login=form_login)
 
 @app.route('/dashboard')
 def dashboard():
